db41a315c736b3f4b4d4f02c9a4f14989486f001
MAGAMBO <93328169+BHAGWANUSERBOT@users.noreply.github.com>
2022-06-12 15:22:02 +0530

    Update filters.py

--- CBAMUSIC/helpers/filters.py
+++ CBAMUSIC/helpers/filters.py
@@ -1,15 +1,19 @@
 from typing import List
+
 from typing import Union
 
-from CBAMUSIC import filters
+from pyrogram import filters
 
 from CBAMUSIC.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+
 other_filters2 = (
+
     filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+
 )
 
-
 def command(commands: Union[str, List[str]]):
+
     return filters.command(commands, COMMAND_PREFIXES)
